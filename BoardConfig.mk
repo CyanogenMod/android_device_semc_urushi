27024a2dfc7827c534b838f9c370c44f5a09e1e1
Michael Bestas <mikeioannina@gmail.com>
2013-03-08 05:59:31 +0200

    urushi: some cleanup
    * Fixed incorrect build fingerprint
    * Added & fixed some comments to the source files
    * Reorganized alphabetically proprietary files list
    * recovery.fstab: convert tabs to spaces
    * Move gsm feature xml to mogami-common
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I2657c0630059ba27d85b9a22e40d6365b9ddbdc4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,15 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/urushi/BoardConfigVendor.mk
 
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_urushi_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+# Recovery
 TARGET_RECOVERY_INITRC := device/semc/urushi/recovery/init.rc
 
+# Sensors
 SENSORS_COMPASS_AK8975 := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
